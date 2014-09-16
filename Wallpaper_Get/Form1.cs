9f90e9f3e014da997b8d80fe339b5d6664cecd66
xjoker <xjoker@xjoker.us>
2014-09-16 10:49:06 +0800

    全自动匹配已知分辨率下载完成

---
+++ Wallpaper_Get/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Wallpaper_Get
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int SW = Screen.PrimaryScreen.Bounds.Width;  
+            Uri uri=null;
+            if(SW.ToString()=="1440")
+            {
+                uri = new Uri("http://10.10.168.80/2000-1440.jpg");
+            }
+            else if  (SW.ToString() == "1024")
+            {
+                uri = new Uri("http://10.10.168.80/2000-1024.jpg");
+            }
+            else if (SW.ToString() == "1366")
+            {
+                uri = new Uri("http://10.10.168.80/2000-1366.jpg");
+            }
+            else
+            {
+                uri = new Uri("http://10.10.168.80/2000.jpg");
+            }
+            getWallpaper.Wallpaper.Set(uri, getWallpaper.Style.Centered);
+            Application.Exit();
+        }
+    }
+}
---
+++ Wallpaper_Get/getWallpaper.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Wallpaper_Get
+{
+    class getWallpaper
+    {
+        public enum Style : int
+        {
+            Tiled,
+            Centered,
+            Stretched
+        }
+        public sealed class Wallpaper
+        {
+            Wallpaper() { }
+            const int SPI_SETDESKWALLPAPER = 20;
+            const int SPIF_UPDATEINIFILE = 0x01;
+            const int SPIF_SENDWININICHANGE = 0x02;
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            //uiAction是作不同的操作参数。
+            //uiParam是设置的参数。
+            //pvParam是设置或返回的参数。
+            //fWinIni是设置的参数。
+            static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+            //壁纸的几种模式的枚举
+            
+            public static void Set(Uri uri, Style style)
+            {
+                try
+                {
+                    System.IO.Stream s = new WebClient().OpenRead(uri.ToString());
+                    //获取壁纸文件，传入到流
+                    System.Drawing.Image img = System.Drawing.Image.FromStream(s);
+                    //传入到image中
+                    string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
+                    //将壁纸的存储地址设置为Temp目录，并且命名为wallpaper.bmp
+                    img.Save(tempPath, System.Drawing.Imaging.ImageFormat.Bmp);
+                    //保存为bmp文件
+                    RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+                    //打开注册表
+                    //判断壁纸设置的类型，并且将键值写入注册表
+                    if (style == Style.Stretched)
+                    {
+                        key.SetValue(@"WallpaperStyle", 2.ToString());
+                        key.SetValue(@"TileWallpaper", 0.ToString());
+                    }
+                    if (style == Style.Centered)
+                    {
+                        key.SetValue(@"WallpaperStyle", 1.ToString());
+                        key.SetValue(@"TileWallpaper", 0.ToString());
+                    }
+                    if (style == Style.Tiled)
+                    {
+                        key.SetValue(@"WallpaperStyle", 1.ToString());
+                        key.SetValue(@"TileWallpaper", 1.ToString());
+                    }
+                    //调用方法设置壁纸
+                    SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                        0,
+                        tempPath,
+                        SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+                }
+                catch
+                {
+
+                }
+                
+            }
+        }
+
+    }
+}
